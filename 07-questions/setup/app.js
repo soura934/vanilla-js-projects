e46cb75c4191da293deb79316abd740f10199780
soura934 <soura934@gmail.com>
2021-07-20 20:08:19 -0400

    add questions project

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", (e)=>{
+        console.log(e);
+        console.log(e.currentTarget);
+    })
+});
